3790f77a96b616d30d1e69972a7c2bb5422223d0
viktor <87v87@mail.ru>
2024-11-05 18:52:45 +0300

    Initial Commit

---
+++ Budgetly/BudgetlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetlyApp.swift
+//  Budgetly
+//
+//  Created by Виктор Корольков on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
